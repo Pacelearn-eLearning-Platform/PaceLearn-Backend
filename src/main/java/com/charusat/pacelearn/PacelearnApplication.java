c0a2bf286c9e7100b0b781ed4512b27815e541f3
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2022-12-28 23:28:56 +0530

    first commit

---
+++ src/main/java/com/charusat/pacelearn/PacelearnApplication.java
@@ -0,0 +1,13 @@
+package com.charusat.pacelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacelearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacelearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charusat/pacelearn/PacelearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charusat.pacelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacelearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

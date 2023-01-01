dd4113e7e1eb1a86ef814edb5b892c085908763e
SoniPratham <d21it173@charusat.edu.in>
2023-01-01 23:23:10 +0530

    basci jwt version

--- src/main/java/com/charusat/pacelearn/Repository/AuthorityRepository.java
+++ src/main/java/com/charusat/pacelearn/Repository/AuthorityRepository.java
@@ -2,6 +2,8 @@ package com.charusat.pacelearn.Repository;
 
 import com.charusat.pacelearn.Entity.Authority;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorityRepository extends CrudRepository<Authority,String> {
 }
--- src/main/java/com/charusat/pacelearn/Repository/UserRepository.java
+++ src/main/java/com/charusat/pacelearn/Repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.charusat.pacelearn.Repository;
 
 import com.charusat.pacelearn.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUserEmail(String email);
-    boolean findByUserEmailExists(String email);
-    Optional<User> findOneByEmailIgnoreCase(String email);
+//    boolean findByUserEmailExists(String email);
+//    Optional<User> findOneByEmailIgnoreCase(String email);
 }

071c2da07938551954cd5e4137d040966c4192f3
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2023-02-23 00:22:34 +0530

    PAC-16 : Modified the registration api in based on charusat email verification version1.1

--- src/main/java/com/charusat/pacelearn/web/rest/AccountResource.java
+++ src/main/java/com/charusat/pacelearn/web/rest/AccountResource.java
@@ -64,7 +64,7 @@ public class AccountResource {
             throw new InvalidPasswordException();
         }
         User user = userService.registerUser(managedUserVM, managedUserVM.getPassword());
-//        mailService.sendActivationEmail(user);
+        mailService.sendActivationEmail(user);
 
         HashMap<String,String> responseBody = new HashMap<>();
         responseBody.put("message","Registered Successfully, please check your email and verify your account before login");
